4a1d97d42a58c32a07be34098242e7cd7435c599
JoseDanielAr <132498954+JoseDanielAr@users.noreply.github.com>
2023-08-03 07:25:11 -0500

    Add files via upload

---
+++ Taller1.py
@@ -0,0 +1,58 @@
+G1=set()
+G2=set()
+switch=0
+
+#Ingresar numeros
+while switch==0:
+    x=float(input("Ingrese un numero decimal para el grupo 1\n"))
+    G1.add(x)
+    R=int(input("¿Añadir otro numero al grupo 1\n1. Sí\n2. No\n"))
+    if R!=1:
+        switch=1
+
+switch=0
+
+print("El grupo 1 es: ", G1 ,"\n")
+while switch==0:
+    x=float(input("Ingrese un numero decimal para el grupo 2\n"))
+    G2.add(x)
+    R=int(input("¿Añadir otro numero al grupo 2\n1. Sí\n2. No\n"))
+    if R!=1:
+        switch=1
+
+switch=0
+
+print("El grupo 2 es: ", G2)
+
+#Operaciones
+while switch==0:
+    R=int(input("\nSeleccione la operación\n1. Union\n2. Intersección\n3. Diferencia (conjunto 1 - conjunto 2)\n4. Diferencia simetrica\n")) 
+
+    if R==1:
+        G3=G1.union(G2)
+        print(G1.union(G2), "cardinalidad: ", len(G3))
+        R=int(input("¿Realizar otra operación?\n1: Sí\nOtro valor: No\n"))
+        if R!=1:
+         switch=1
+    elif R==2:
+        G3=G1.intersection(G2)
+        print(G1.intersection(G2), "cardinalidad: ", len(G3))
+        R=int(input("¿Realizar otra operación?\n1: Sí\nOtro valor: No\n"))
+        if R!=1:
+         switch=1
+    elif R==3:
+        G3=G1.difference(G2)
+        print(G1.difference(G2), "cardinalidad: ", len(G3))
+        R=int(input("¿Realizar otra operación?\n1: Sí\nOtro valor: No\n"))
+        if R!=1:
+         switch=1
+    elif R==4:
+        G3=G1.symmetric_difference(G2)
+        print(G1.symmetric_difference(G2), "cardinalidad: ", len(G3))
+        R=int(input("¿Realizar otra operación?\n1: Sí\nOtro valor: No\n"))
+        if R!=1:
+         switch=1
+    else:
+        print("Comando invalido")
+
+
